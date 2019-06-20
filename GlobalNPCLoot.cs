23aeea8c66d84c00bf9ce14fe5172bdb785a6a2b
Wingmann <shamanwingman@gmail.com>
2019-06-20 21:04:35 +1200

    Fixed drop rates

--- GlobalNPCLoot.cs
+++ GlobalNPCLoot.cs
@@ -14,26 +14,26 @@ namespace Aetherium
     {
         public override void NPCLoot(NPC npc)
         {
-            if (npc.damage > 1 && npc.life > 1)
+            if (npc.damage > 1 && npc.lifeMax > 1)
             {
-                if (npc.position.Y > Main.worldSurface && npc.position.Y < Main.worldSurface * 0.35)
+                if (npc.position.Y > Main.worldSurface && Main.dayTime)
                 {
-                    if (Main.rand.Next(50) == 0)
+                    if (Main.rand.Next(250) == 0)
                     {
                         Item.NewItem(npc.getRect(), mod.ItemType("Jade_Quiver"));
                     }
-                    if (Main.rand.Next(65) == 0)
+                    if (Main.rand.Next(250) == 0)
                     {
                         Item.NewItem(npc.getRect(), mod.ItemType("Guardians_Courage"));
                     }
-                    if (!Main.dayTime && Main.rand.Next(50) == 0)
+                    if (!Main.dayTime && Main.rand.Next(250) == 0)
                     {
                         Item.NewItem(npc.getRect(), mod.ItemType("Vampire_Charm"));
                     }
                 }
-                if (npc.position.Y < Main.worldSurface && npc.position.Y > Main.maxTilesY - 200)
+                if (npc.position.Y < Main.worldSurface)
                 {
-                    if (Main.rand.Next(50) == 0)
+                    if (Main.rand.Next(250) == 0)
                     {
                         Item.NewItem(npc.getRect(), mod.ItemType("Pirates_Coinpurse"));
                     }
