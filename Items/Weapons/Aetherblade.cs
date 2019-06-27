690a8eb88bb08150c4135060ba5bdf860f0787aa
Wingmann <shamanwingman@gmail.com>
2019-06-27 17:40:09 +1200

    test

--- Items/Weapons/Aetherblade.cs
+++ Items/Weapons/Aetherblade.cs
@@ -10,25 +10,25 @@ using Microsoft.Xna.Framework;
 
 namespace Aetherium.Items.Weapons
 {
-    public class Aetherium_Sword : ModItem
+    public class Aetherblade : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Aetherium Sword");
+            DisplayName.SetDefault("Aetherblade");
         }
         public override void SetDefaults()
         {
-            item.damage = 14;           //The damage of your weapon
+            item.damage = 140;           //The damage of your weapon
             item.melee = true;          //Is your weapon a melee weapon?
-            item.width = 32;            //Weapon's texture's width
-            item.height = 32;           //Weapon's texture's height
-            item.useTime = 25;          //The time span of using the weapon. Remember in terraria, 60 frames is a second.
-            item.useAnimation = 25;         //The time span of the using animation of the weapon, suggest set it the same as useTime.
+            item.width = 58;            //Weapon's texture's width
+            item.height = 58;           //Weapon's texture's height
+            item.useTime = 20;          //The time span of using the weapon. Remember in terraria, 60 frames is a second.
+            item.useAnimation = 20;         //The time span of the using animation of the weapon, suggest set it the same as useTime.
             item.useStyle = 1;          //The use style of weapon, 1 for swinging, 2 for drinking, 3 act like shortsword, 4 for use like life crystal, 5 for use staffs or guns
             item.knockBack = 6;         //The force of knockback of the weapon. Maximum is 20
             item.value = Item.buyPrice(silver: 50);           //The value of the weapon
             item.rare = 9;              //The rarity of the weapon, from -1 to 13
-            item.UseSound = SoundID.Item1;      //The sound when the weapon is using
+            item.UseSound = SoundID.Item1; // test      //The sound when the weapon is using
             item.autoReuse = true; //Whether the weapon can use automatically by pressing mousebutton
         }
 
@@ -36,7 +36,14 @@ namespace Aetherium.Items.Weapons
         {
             for (int i = 0; i < 6; i++)
             {
-                Dust.NewDust(target.position, target.width, target.height, DustID.BubbleBlock);
+                Dust.NewDust(target.position, target.width, target.height, 15);
+            }
+        }
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            if (Main.rand.NextBool(2))
+            {
+                Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 15);
             }
         }
 
--- README.md
+++
@@ -1,2 +0,0 @@
-# Aetherium
-Aetherium - a WIP tModLoader mod focused around adding items &amp; NPCs with interesting mechanics - rather than just more of the same.
